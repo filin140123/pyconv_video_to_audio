8ac1eda9fe82f54d803896c093a653b12520dbc3
drakehitch <filin140123@gmail.com>
2022-09-26 12:41:03 +0500

    size of subclip update

--- converter.py
+++ converter.py
@@ -1,4 +1,13 @@
 import moviepy.editor as mp
+from math import ceil
+
 name = input("Enter video name with ext: ")
-clip = mp.VideoFileClip(name).subclip(0, 4116)
-clip.audio.write_audiofile(f"audio-{name.split('.')[0]}.mp3")
+clip = mp.VideoFileClip(name)
+
+start_input = input("Start encoding with this time (mm:ss): ")
+m, s = [int(i) for i in start_input.split(":")]
+start_seconds = m * 60 + s
+end_seconds = ceil(clip.duration)
+
+outclip = clip.subclip(start_seconds, end_seconds)
+outclip.audio.write_audiofile(f"audio-{name.split('.')[0]}.mp3")
